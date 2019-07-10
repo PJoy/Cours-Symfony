ed0a6b1f78beca783ca8315694caa6f52be6efff
thouni2009 <36662013+thouni2009@users.noreply.github.com>
2019-07-10 20:25:46 +0200

    TP autonomie terminé à 80%
    Slug non fonctionnel
    Impossibilité d'uploader les dossier "var" et "vendor"

---
+++ TP/src/Entity/Tag.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Product", inversedBy="tags")
+     */
+    private $relation;
+
+    public function __construct()
+    {
+        $this->relation = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getRelation(): Collection
+    {
+        return $this->relation;
+    }
+
+    public function addRelation(Product $relation): self
+    {
+        if (!$this->relation->contains($relation)) {
+            $this->relation[] = $relation;
+        }
+
+        return $this;
+    }
+
+    public function removeRelation(Product $relation): self
+    {
+        if ($this->relation->contains($relation)) {
+            $this->relation->removeElement($relation);
+        }
+
+        return $this;
+    }
+}
